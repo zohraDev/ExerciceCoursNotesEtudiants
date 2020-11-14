1a4349c51fe9fb49b0e75cc48e54c7fe4d126767
zohraDev <bach.zora@hotmail.fr>
2020-11-14 23:22:49 +0100

    premier commit

---
+++ src/com/game/projet/company/Main.java
@@ -0,0 +1,25 @@
+package com.game.projet.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+	// write your code here
+    }
+
+
+
+   public static int inputStudents(){
+       Scanner sc =new Scanner(System.in);
+        System.out.println("Enter number of students");
+
+
+       return 1;
+   }
+
+
+}
